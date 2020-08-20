232973c8bac0a3b77da80106708843965307a010
benihime91 <aburagohain160@gmail.com>
2020-08-21 01:24:03 +0530

    :<

--- train.py
+++ train.py
@@ -4,5 +4,12 @@ from src.models import Retinanet
 from src.trainer import DefaultTrainer
 
 
-model = Retinanet()
-print(model)
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-lr", "--learning_rate", type=float, default=1e-03, help="optimizer learning rate"
+)
+parser.add_argument("-mom", "--momentum", type=float, default=0.9, help="momentum")
+parser.add_argument(
+    "-wd", "--weight_decay", type=float, default=1e-02, help="optimizer weight decay"
+)
+parser.add_argument("-e", "--epochs", type=int, default=30, help="# epochs")
