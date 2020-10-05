600e02d7f47c3b2c858b6ab9169726c409225ce2
benihime91 <aburagohain160@gmail.com>
2020-10-05 09:40:54 +0530

    Update README.md

--- README.md
+++ README.md
@@ -11,6 +11,8 @@ The code is heavily influended by [Detectron2](https://github.com/facebookresear
 - [x] Assemble Retinanet Using `BackBone` => `FeaturePyramid` => `AnchorGenerator` => `Regeression` & `Classification`.
 
 ## Tutorials:
+- For a example training pipeline see : [001_demo.ipynb](https://github.com/benihime91/pytorch_retinanet/blob/master/001_demo.ipynb)
+- Dataset Loading and visualization examples: [000_demo.ipynb](https://github.com/benihime91/pytorch_retinanet/blob/master/001_demo.ipynb)
 
 ## Usage:
    Clone the Github Repo
@@ -62,3 +64,132 @@ The code is heavily influended by [Detectron2](https://github.com/facebookresear
     # to test model using COCO API
     trainer.test(model)
    ```
+
+## Loading Data:
+
+The data can be loaded into the model in one of 3 ways.  
+
+This is controlled via the `dataset.kind` parameter in `hparams.yaml`. 
+
+1. To load in the in the COCO-2017 dataset: 
+   * set `dataset.kind` = "coco"
+   * set `dataset.root_dir` = {path to the coco dataset}
+  
+   ```yaml
+   dataset:
+      kind: coco
+      root_dir: /Datasets/coco/
+   ```
+
+2. If the dataset is in Pascal-VOC format :
+   * set  `dataset.kind` = "pascal"
+   * set `data.trn_paths` = [path_to_annotations, path_to_images]
+   * similarly set the paths for valiation and test, test paths are optional. The dataset from the test paths is used to perform COCO-API-evaluation . If no test paths are given then COCO-API-evaluation is instead done on the datasdet generated from val_paths
+
+   ```yaml
+   dataset:
+      kind: pascal
+      trn_paths:
+         - "/content/pascal_voc_2007_train_val/Annotations/"
+         - /content/pascal_voc_2007_train_val/Images/"
+      val_paths:
+         - /content/pascal_voc_2007_test/Annotations/"
+         - /content/pascal_voc_2007_test/Images/"
+      test_paths: false
+   ```
+
+3. The datasets can also be loaded from a csv format. The csv file should be as follows :
+   ```
+   filename,width,height,class,xmin,ymin,xmax,ymax,labels
+   Images/007826.jpg,500,375,diningtable,80,217,320,273,11
+   Images/007826.jpg,500,375,chair,197,193,257,326,9
+   ...
+   ...
+   Images/006286.jpg,500,375,diningtable,402,219,500,375,11
+   Images/006286.jpg,500,375,diningtable,347,177,405,216,11
+   ```
+   * **`filename`** : path to the Image
+   * **`width`** : width of the Image [Optional] 
+   * **`height`** : height of the Image [Optional] 
+   * **`class`** : class label for the particular annotation
+   * **`labels`** : integer labels for the particular annotation
+   * **`xmin`**, **`ymin`**, **`xmax`**, **`ymax`**: absolute bounding-box co-ordinates
+
+   Note : 
+   - each entry in the csv file should corresponding to a unique bounding-box.
+   - labels should start from 1 as the 0th label is reserved for "__background__" class.
+   - to generate a `LABEL_MAP` to be used for visulazation purposes:
+    ```python
+    from utils.pascal import generate_pascal_category_names
+    import pandas as pd
+    
+    path = ... # path to csv file contraining the annotations
+    df = pd.read_csv(path)
+    
+    # Generate a label map
+    LABEL_MAP = generate_pascal_category_names(df)
+    ```
+
+   ```yaml
+   dataset:
+      kind: pascal
+      trn_paths: "train_data.csv"
+      val_paths: "val_data.csv"
+      test_paths: "test_data.csv" # this is Optional
+   ```
+
+
+## Visualizing the bounding-box(s) over the image :
+
+   ```python
+   from utils import visualize_boxes_and_labels_on_image_array
+   from PIL import Image
+   import cv2
+   import numpy as np
+
+   image = cv2.cvtColor(cv2.imread(image_path), cv2.COLOR_BGR2RGB)
+   
+   # or :
+   # image = Image.open(image_path)
+   # image = np.array(image)
+
+   # normalize the image
+   image = image / 255.0
+
+   # label map should be a list containing contraining the name
+   # of the categories. Each cotegory should be at the index 
+   # corresponding to the integer category
+   # 0th index is reserved for the background class
+   LABEL_MAP = ...
+
+   # (N,4) dimensional array containing the absolute bounding-box
+   # co-ordinates in xmin, ymin, xmax, ymax, format. 
+   boxes = ...
+   # (N) dimensional array contraining the interger labels
+   labels = ...
+   # (N) dimensional array contraining the confidence probability for the image. 
+   # This can also be None.
+   scores = ...
+
+   # draw bounding-box over the loaded image
+   im = visualize_boxes_and_labels_on_image_array(image,
+                                                  boxes,
+                                                  labels,
+                                                  scores,
+                                                  LABEL_MAP
+                                                  )
+   # this function returns a PIL image instance 
+   # to view the image
+   im.show()
+   # or in jupyter-notebooks use : im
+   ```
+
+## References : 
+```bash
+https://arxiv.org/abs/1708.02002
+https://github.com/facebookresearch/detectron2
+https://github.com/pytorch/vision
+https://github.com/fastai/course-v3/blob/master/nbs/dl2/pascal.ipynb
+https://github.com/tensorflow/models/tree/master/research/object_detection
+https://github.com/PyTorchLightning/wheat/tree/dee605b0bf5cf6b0ab08755c45e38dc07d338bb7
+```
--- model.py
+++ model.py
@@ -7,15 +7,17 @@ from torch.utils.data import DataLoader
 
 from retinanet import Retinanet
 from utils import _get_logger, collate_fn, load_obj
+
 # COCO Utils
 from utils.coco import CocoEvaluator, get_coco, get_coco_api_from_dataset
 from utils.coco.coco_transforms import Compose, RandomHorizontalFlip, ToTensor
+
 # PASCAL UTILS
-from utils.pascal import get_pascal
+from utils.pascal import get_pascal, PascalDataset
 from utils.pascal.pascal_transforms import compose_transforms
 
 
-def _get_model(hparams:DictConfig, **kwargs):
+def _get_model(hparams: DictConfig, **kwargs):
     model = Retinanet(**hparams.model, **kwargs)
     return model
 
@@ -35,7 +37,7 @@ class RetinaNetModel(pl.LightningModule):
         # load model using model hparams
         self.fancy_logger = _get_logger(__name__)
         self.model = _get_model(self.hparams, logger=self.fancy_logger)
-        
+
     def forward(self, xb, *args, **kwargs):
         return self.model(xb)
 
@@ -51,21 +53,61 @@ class RetinaNetModel(pl.LightningModule):
             trn_tfms = Compose([ToTensor(), RandomHorizontalFlip(prob=0.5)])
             val_tfms = Compose([ToTensor()])
             # Load in the COCO Train and Validation datasets
-            self.trn_ds = get_coco(root=data_params.root_dir, image_set="train", transforms=trn_tfms)
-            self.val_ds = get_coco(root=data_params.root_dir, image_set="val", transforms=val_tfms)
+            self.trn_ds = get_coco(
+                root=data_params.root_dir, image_set="train", transforms=trn_tfms
+            )
+            self.val_ds = get_coco(
+                root=data_params.root_dir, image_set="val", transforms=val_tfms
+            )
             self.test_ds = False
 
         # if pascal-dataset format load in the pascal dataset
         elif data_params.kind == "pascal":
-            trn_tfms = [load_obj(i["class_name"])(**i["params"])for i in self.hparams.pascal_transforms]
+            trn_tfms = [
+                load_obj(i["class_name"])(**i["params"])
+                for i in self.hparams.transforms
+            ]
             trn_tfms = compose_transforms(trn_tfms)
             val_tfms = compose_transforms()
             # Load in the pascal dataset from the csv files
-            self.trn_ds = get_pascal(data_params.trn_paths[0], data_params.trn_paths[1],"train", transforms=trn_tfms,)
-            self.val_ds = get_pascal(data_params.valid_paths[0], data_params.valid_paths[1], "test", transforms=val_tfms,)
+            self.trn_ds = get_pascal(
+                data_params.trn_paths[0],
+                data_params.trn_paths[1],
+                "train",
+                transforms=trn_tfms,
+            )
+
+            self.val_ds = get_pascal(
+                data_params.valid_paths[0],
+                data_params.valid_paths[1],
+                "test",
+                transforms=val_tfms,
+            )
 
             if data_params.test_paths:
-                self.test_ds = get_pascal(data_params.test_paths[0], data_params.test_paths[1],"test", transforms=val_tfms,)
+                self.test_ds = get_pascal(
+                    data_params.test_paths[0],
+                    data_params.test_paths[1],
+                    "test",
+                    transforms=val_tfms,
+                )
+            else:
+                self.test_ds = False
+
+        # if csv-data format
+        elif data_params.kind == "csv":
+            trn_tfms = [
+                load_obj(i["class_name"])(**i["params"])
+                for i in self.hparams.transforms
+            ]
+            trn_tfms = compose_transforms(trn_tfms)
+            val_tfms = compose_transforms()
+            # load in the datasets from the csv files
+            self.trn_ds = PascalDataset(data_params.trn_paths, trn_tfms)
+            self.val_ds = PascalDataset(data_params.valid_paths, val_tfms)
+
+            if data_params.test_paths:
+                self.test_ds = PascalDataset(data_params.test_paths, val_tfms)
             else:
                 self.test_ds = False
 
@@ -81,37 +123,62 @@ class RetinaNetModel(pl.LightningModule):
         self.scheduler = load_obj(self.hparams.scheduler.class_name)(
             self.optimizer, **self.hparams.scheduler.params
         )
-        self.scheduler = {
-            "scheduler": self.scheduler,
-            "interval": self.hparams.scheduler.interval,
-            "frequency": self.hparams.scheduler.frequency,
-        }
+
+        if not self.hparams.scheduler.monitor:
+            self.scheduler = {
+                "scheduler": self.scheduler,
+                "interval": self.hparams.scheduler.interval,
+                "frequency": self.hparams.scheduler.frequency,
+            }
+        else:
+            self.scheduler = {
+                "scheduler": self.scheduler,
+                "interval": self.hparams.scheduler.interval,
+                "frequency": self.hparams.scheduler.frequency,
+                "monitor": self.hparams.scheduler.monitor,
+            }
 
         # log optimizer and scheduler
         self.fancy_logger.info(f"OPTIMIZER_NAME : {self.optimizer.__class__.__name__}")
         self.fancy_logger.info(f"LEARNING_RATE: {self.hparams.optimizer.params.lr}")
-        self.fancy_logger.info(f"WEIGHT_DECAY: {self.hparams.optimizer.params.weight_decay}")
-        self.fancy_logger.info(f"LR_SCHEDULER_NAME : {self.scheduler['scheduler'].__class__.__name__}")
+        self.fancy_logger.info(
+            f"WEIGHT_DECAY: {self.hparams.optimizer.params.weight_decay}"
+        )
+        self.fancy_logger.info(
+            f"LR_SCHEDULER_NAME : {self.scheduler['scheduler'].__class__.__name__}"
+        )
         return [self.optimizer], [self.scheduler]
 
     def train_dataloader(self, *args, **kwargs):
         bs = self.hparams.dataloader.train_bs
-        loader = DataLoader(self.trn_ds,bs,shuffle=True,collate_fn=collate_fn,**self.hparams.dataloader.args,)
+        loader = DataLoader(
+            self.trn_ds,
+            bs,
+            shuffle=True,
+            collate_fn=collate_fn,
+            **self.hparams.dataloader.args,
+        )
         return loader
 
     def val_dataloader(self, *args, **kwargs):
         bs = self.hparams.dataloader.valid_bs
-        loader = DataLoader(self.val_ds, bs, collate_fn=collate_fn, **self.hparams.dataloader.args)
+        loader = DataLoader(
+            self.val_ds, bs, collate_fn=collate_fn, **self.hparams.dataloader.args
+        )
         return loader
 
     def test_dataloader(self, *args, **kwargs):
         if not self.test_ds:
             bs = self.hparams.dataloader.valid_bs
-            loader = DataLoader(self.val_ds, bs, collate_fn=collate_fn, **self.hparams.dataloader.args)
+            loader = DataLoader(
+                self.val_ds, bs, collate_fn=collate_fn, **self.hparams.dataloader.args
+            )
         else:
             bs = self.hparams.dataloader.test_bs
-            loader = DataLoader(self.test_ds, bs, collate_fn=collate_fn, **self.hparams.dataloader.args)
-        
+            loader = DataLoader(
+                self.test_ds, bs, collate_fn=collate_fn, **self.hparams.dataloader.args
+            )
+
         # instantiate coco_api to track metrics
         prompt = "Converting dataset annotations in 'test_dataset' to COCO format for inference ..."
         self.fancy_logger.info(prompt)
@@ -139,7 +206,11 @@ class RetinaNetModel(pl.LightningModule):
         loss = sum(loss for loss in loss_dict.values())
         loss = torch.as_tensor(loss)
         logs = {"val_loss": loss}
-        return {"val_loss": loss, "log": logs, "progress_bar": logs,}
+        return {
+            "val_loss": loss,
+            "log": logs,
+            "progress_bar": logs,
+        }
 
     def test_step(self, batch, batch_idx, *args, **kwargs):
         images, targets, _ = batch
@@ -158,13 +229,18 @@ class RetinaNetModel(pl.LightningModule):
         metric = self.test_evaluator.coco_eval["bbox"].stats[0]
         metric = torch.as_tensor(metric)
         logs = {"AP": metric}
-        return {"AP": metric, "log": logs, "progress_bar": logs,}
+        return {
+            "AP": metric,
+            "log": logs,
+            "progress_bar": logs,
+        }
 
 
 class LogCallback(pl.Callback):
     """
     Callback to handle logging within pl_module
     """
+
     def on_fit_start(self, trainer, pl_module):
         eps = trainer.max_epochs
         pl_module.fancy_logger.info(f"MAX_EPOCHS : {eps}")
@@ -183,7 +259,9 @@ class LogCallback(pl.Callback):
 
     def on_test_start(self, trainer, pl_module):
         self.test_start = datetime.datetime.now().replace(microsecond=0)
-        prompt = (f"Start Inference on {pl_module.test_dataloader().dataset.__len__()} images")
+        prompt = (
+            f"Start Inference on {pl_module.test_dataloader().dataset.__len__()} images"
+        )
         pl_module.fancy_logger.info(prompt)
 
     def on_test_end(self, trainer, pl_module):
