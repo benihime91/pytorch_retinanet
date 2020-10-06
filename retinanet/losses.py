remove bug related to predictions and loss calculation

retinanet/losses.py
@@ -32,9 +32,9 @@ class RetinaNetLosses(nn.Module):
         
         Args:
             1. clas_pred: A float tensor of arbitrary shape.
-                       The predictions for each example.
+                         The predictions for each example.
             2. clas_tgt: A float tensor with the same shape as inputs.
-                        Stores the binary classification label for each element in inputs
+                         Stores the binary classification label for each element in inputs
                         (0 for the negative class and 1 for the positive class).
         Returns:
             Loss tensor
@@ -77,7 +77,10 @@ class RetinaNetLosses(nn.Module):
         clas_mask = matches >= 0
         clas_pred = clas_pred[clas_mask]
 
-        # convert predictions from [0, num_classes) -> [1, num_classes]
+        # model is going to predict classes which are going to be in the range of [0, num_classes]
+        # 0 is reserved for the background class for which no loss is calculate , so 
+        # we will add 1 to all the class_predictions to shift the predicitons range from
+        # [0, num_classes) -> [1, num_classes]
         clas_pred = clas_pred + 1
 
         # # clas_tgt : [0, num_classes) -> [1, num_classes]

retinanet/models.py
@@ -1,5 +1,4 @@
 import logging
-from logging import Logger
 from typing import *
 
 import torch
@@ -232,7 +231,7 @@ class Retinanet(nn.Module):
             all_labels = all_labels + 1 
             
             # Sort by scores and 
-            #  Grab the idxs from the corresponding to the topk predictions
+            # Grab the idxs from the corresponding to the topk predictions
             _, topk_idxs = all_scores.sort(descending=True)
             topk_idxs = topk_idxs[: self.detections_per_img]
             all_boxes, all_scores, all_labels = (
@@ -283,7 +282,6 @@ class Retinanet(nn.Module):
         outputs = self.retinanet_head(feature_maps)
         # Generate anchors for the images
         anchors = self.anchor_generator(images, feature_maps)
-
         # store losses
         losses = {}
         losses = self.compute_loss(targets, outputs, anchors)