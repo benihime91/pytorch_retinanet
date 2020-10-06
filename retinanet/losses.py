remove bug related to predictions and loss calculation

retinanet/losses.py
@@ -11,10 +11,10 @@ from .config import *
 class RetinaNetLosses(nn.Module):
     def __init__(self, num_classes: int) -> None:
         super(RetinaNetLosses, self).__init__()
-        self.n_c = num_classes
+        self.n_c   = num_classes
         self.alpha = FOCAL_LOSS_ALPHA
         self.gamma = FOCAL_LOSS_GAMMA
-        self.beta = SMOOTH_L1_LOSS_BETA
+        self.beta  = SMOOTH_L1_LOSS_BETA
 
     def smooth_l1_loss(self, input: Tensor, target: Tensor) -> torch.Tensor:
         """Computes SmoothL1Loss"""
@@ -28,7 +28,7 @@ class RetinaNetLosses(nn.Module):
 
     def focal_loss(self, clas_pred: Tensor, clas_tgt: Tensor) -> Tensor:
         """
-        Loss used in RetinaNet for dense detection: https://arxiv.org/abs/1708.02002.
+        Focal Loss used in RetinaNet: https://arxiv.org/abs/1708.02002.
         
         Args:
             1. clas_pred: A float tensor of arbitrary shape.
@@ -43,9 +43,7 @@ class RetinaNetLosses(nn.Module):
         weights = clas_tgt * (1 - ps) + (1 - clas_tgt) * ps
         alphas = (1 - clas_tgt) * self.alpha + clas_tgt * (1 - self.alpha)
         weights.pow_(self.gamma).mul_(alphas)
-        clas_loss = F.binary_cross_entropy_with_logits(
-            clas_pred, clas_tgt, weights, reduction="sum"
-        )
+        clas_loss = F.binary_cross_entropy_with_logits(clas_pred, clas_tgt, weights, reduction="sum")
         return clas_loss
 
     def calc_loss(
@@ -79,13 +77,18 @@ class RetinaNetLosses(nn.Module):
         clas_mask = matches >= 0
         clas_pred = clas_pred[clas_mask]
 
+        # convert predictions from [0, num_classes) -> [1, num_classes]
+        clas_pred = clas_pred + 1
+
         # # clas_tgt : [0, num_classes) -> [1, num_classes]
         # clas_tgt = clas_tgt + 1
         # # no need to add +1 since clas_tgt: [1, num_classes]
         # Add background class to account for background in `matches`.
 
         # When there are no matches
-        # bg class is predicted when none of the others go out.
+        # bg class is predicted so, we need to add the backgorund class
+        # to each of the targets , or else there will be index error, as
+        # bboxes are predicted for background class as well
         # add 0 label for the background class
         clas_tgt = torch.cat([clas_tgt.new_zeros(1).long(), clas_tgt])
         clas_tgt = clas_tgt[matches[clas_mask]]
@@ -117,18 +120,13 @@ class RetinaNetLosses(nn.Module):
         classification_losses = []
         regression_losses = []
 
-        for cls_pred, bb_pred, targs, ancs in zip(
-            clas_preds, bbox_preds, targets, anchors
-        ):
-
+        for cls_pred, bb_pred, targs, ancs in zip(clas_preds, bbox_preds, targets, anchors):
             # Extract the Labels & boxes from the targets
             class_targs, bbox_targs = targs["labels"], targs["boxes"]
-
-            # Compute loss
-            regression_loss, classification_loss = self.calc_loss(
-                ancs, cls_pred, bb_pred, class_targs, bbox_targs
-            )
-
+            # Compute losses
+            losses = self.calc_loss(ancs,cls_pred, bb_pred, class_targs, bbox_targs)
+            # unpack the losses                                                     
+            regression_loss, classification_loss = losses
             # Append Losses of all the batches
             classification_losses.append(classification_loss)
             regression_losses.append(regression_loss)

retinanet/models.py
@@ -182,33 +182,34 @@ class Retinanet(nn.Module):
         for bb_per_im, sc_per_im, ancs_per_im, im_sz, lbl_per_im in zip(
             bboxes, scores, anchors, im_szs, labels
         ):
-            all_boxes = []
+            all_boxes  = []
             all_scores = []
             all_labels = []
             # convert the activation i.e, outputs of the model to bounding boxes
             bb_per_im = activ_2_bbox(bb_per_im, ancs_per_im)
-            # clip the bounding -boxes to the image size
+            # clip the bounding boxes to the image size
             bb_per_im = ops.clip_boxes_to_image(bb_per_im, im_sz)
 
             # Iterate over each `cls_idx` in `num_classes` and do nms
+            # to each class individually
             for cls_idx in range(num_classes):
-                # remove low scoring boxes and grab the predictions corresponding to the cls_idx
+                # remove low predicitons with scores < score_thres
+                #  and grab the predictions corresponding to the cls_idx
                 inds = torch.gt(sc_per_im[:, cls_idx], self.score_thres)
                 bb_per_cls, sc_per_cls, lbl_per_cls = (
                     bb_per_im[inds],
                     sc_per_im[inds, cls_idx],
                     lbl_per_im[inds, cls_idx],
                 )
-                # remove empty boxes
+                # remove boxes that are too small ~(1-02)
                 keep = ops.remove_small_boxes(bb_per_cls, min_size=1e-2)
                 bb_per_cls, sc_per_cls, lbl_per_cls = (
                     bb_per_cls[keep],
                     sc_per_cls[keep],
                     lbl_per_cls[keep],
                 )
-                # non-maximum suppression, independently done per class
+                # compute non max supression to supress overlapping boxes
                 keep = ops.nms(bb_per_cls, sc_per_cls, self.nms_thres)
-
                 bb_per_cls, sc_per_cls, lbl_per_cls = (
                     bb_per_cls[keep],
                     sc_per_cls[keep],
@@ -220,12 +221,19 @@ class Retinanet(nn.Module):
                 all_labels.append(lbl_per_cls)
 
             # Convert to tensors
-            all_boxes = torch.cat(all_boxes, dim=0)
+            all_boxes  = torch.cat(all_boxes, dim=0)
             all_scores = torch.cat(all_scores, dim=0)
             all_labels = torch.cat(all_labels, dim=0)
-            # Sort by scores
+            
+            # model is going to predict classes which are going to be in the range of [0, num_classes]
+            # 0 is reserved for the background class for which no loss is calculate , so 
+            # we will add 1 to all the class_predictions to shift the predicitons range from
+            # [0, num_classes) -> [1, num_classes]
+            all_labels = all_labels + 1 
+            
+            # Sort by scores and 
+            #  Grab the idxs from the corresponding to the topk predictions
             _, topk_idxs = all_scores.sort(descending=True)
-            # Grab the idxs from the corresponding to the topk predictions
             topk_idxs = topk_idxs[: self.detections_per_img]
             all_boxes, all_scores, all_labels = (
                 all_boxes[topk_idxs],
@@ -233,10 +241,7 @@ class Retinanet(nn.Module):
                 all_labels[topk_idxs],
             )
 
-            detections.append(
-                {"boxes": all_boxes, "scores": all_scores, "labels": all_labels,}
-            )
-
+            detections.append({"boxes": all_boxes, "scores": all_scores, "labels": all_labels,})
         return detections
 
     def predict(self, images: List[Tensor]) -> List[Dict[str, Tensor]]: